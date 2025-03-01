ca9fdd2c6ad76d8d03d75eb2c93e20532c8f19a9
David Chen <chen7daivd@me.com>
2025-03-01 15:15:33 -0500

    chore: setup path aliasing at runtime

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { add, divide, multiply, subtract } from "@src/utils/math.util";
+
+const result = add(1, 2);
+console.log(result);
---
+++ src/utils/math.util.ts
@@ -0,0 +1,20 @@
+export const add = (a: number, b: number): number => a + b;
+export const subtract = (a: number, b: number): number => a - b;
+export const multiply = (a: number, b: number): number => a * b;
+export const divide = (a: number, b: number): number => a / b;
+
+export const power = (a: number, b: number): number => a ** b;
+export const squareRoot = (a: number): number => a ** (1 / 2);
+export const cubeRoot = (a: number): number => a ** (1 / 3);
+
+export const absoluteValue = (a: number): number => Math.abs(a);
+export const sign = (a: number): number => Math.sign(a);
+export const max = (a: number, b: number): number => Math.max(a, b);
+export const min = (a: number, b: number): number => Math.min(a, b);
+export const random = (a: number, b: number): number =>
+  Math.random() * (b - a) + a;
+
+export const factorial = (n: number): number => {
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
+};
