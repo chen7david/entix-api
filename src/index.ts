af8bc3ac0271112a24dfc40977d55a74dea064c4
David Chen <chen7daivd@me.com>
2025-03-09 21:56:00 +0000

    chore: removed example tests

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { add, divide, multiply, subtract } from "@src/utils/math.util";
 import { env } from "./config/env.config";
 
-const result = add(1, 2);
-console.log(result);
 console.log(env);
