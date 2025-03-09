a12805376746cd5cb54a43afecf04d03f7b7d0eb
David Chen <chen7daivd@me.com>
2025-03-09 19:45:29 +0000

    chore: updated db env vars in db connection file

--- src/db/connection.db.ts
+++ src/db/connection.db.ts
@@ -4,11 +4,11 @@ import { Environment } from "@src/types/app.types";
 
 const getDbConfig = (): PoolConfig => {
   const baseConfig = {
-    host: env.DB_HOST,
-    port: env.DB_PORT,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME,
+    host: env.POSTGRES_HOST,
+    port: env.POSTGRES_PORT,
+    user: env.POSTGRES_USER,
+    password: env.POSTGRES_PASSWORD,
+    database: env.POSTGRES_DB,
     connectionTimeoutMillis: 5000,
     max: 20,
     idleTimeoutMillis: 30000,
