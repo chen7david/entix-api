dc34b4b39e4c83e6ab0c5244d52130834b3c3714
David Chen <chen7daivd@me.com>
2025-03-18 02:42:25 +0000

    chore: updated tests

--- src/utils/example.util.test.ts
+++ src/utils/example.util.test.ts
@@ -6,11 +6,12 @@ describe('example.util', () => {
   });
 
   it('db env should be devined and correct', () => {
-    expect(process.env.DB_NAME).toBe('postgres');
-    expect(process.env.DB_PASSWORD).toBe('postgres');
-    expect(process.env.DB_USER).toBe('postgres');
-    expect(process.env.DB_HOST).toBe('localhost');
-    expect(process.env.DB_PORT).toBe('5432');
-    expect(process.env.MAX_POOL_SIZE).toBe('2');
+    const DEFINED = '***'; // gitub returns "***" if a secret is defined
+    expect(process.env.DB_NAME).toBe(DEFINED);
+    expect(process.env.DB_PASSWORD).toBe(DEFINED);
+    expect(process.env.DB_USER).toBe(DEFINED);
+    expect(process.env.DB_HOST).toBe(DEFINED);
+    expect(process.env.DB_PORT).toBe(DEFINED);
+    expect(process.env.MAX_POOL_SIZE).toBe(DEFINED);
   });
 });
