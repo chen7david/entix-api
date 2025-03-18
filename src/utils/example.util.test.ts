cebc21027d7d12b7739b800bfb0c2b0e9bd28cbe
David Chen <chen7daivd@me.com>
2025-03-18 02:33:50 +0000

    chore: added test github action with example test

--- src/utils/example.util.test.ts
+++ src/utils/example.util.test.ts
@@ -4,4 +4,13 @@ describe('example.util', () => {
   it('add', () => {
     expect(add(1, 2)).toBe(3);
   });
+
+  it('db env should be devined and correct', () => {
+    expect(process.env.DB_NAME).toBe('postgres');
+    expect(process.env.DB_PASSWORD).toBe('postgres');
+    expect(process.env.DB_USER).toBe('postgres');
+    expect(process.env.DB_HOST).toBe('localhost');
+    expect(process.env.DB_PORT).toBe('5432');
+    expect(process.env.MAX_POOL_SIZE).toBe('2');
+  });
 });
