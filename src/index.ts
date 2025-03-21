3f092eee3328e65e37a15bee2cc51f49f68bc8ce
David Chen <chen7daivd@me.com>
2025-03-21 18:10:08 +0000

    chore: removed obsolete files

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { env } from '@src/config/env.config';
-import { logger } from '@src/services/logger.service';
-
-logger.info('Application initialized', {
-  environment: env.NODE_ENV,
-  appName: env.APP_NAME,
-  newRelicEnabled: env.NEW_RELIC_ENABLED,
-  someCustomField: 'someCustomValue',
-});
