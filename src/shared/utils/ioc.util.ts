bf45c4c8959b0a8ea07a8df29daccb0975900986
David Chen <chen7daivd@me.com>
2025-06-03 16:03:39 -0400

    chore: clean up and prepare for integration

--- src/shared/utils/ioc.util.ts
+++
@@ -1,7 +0,0 @@
-/**
- * IoC utility module: re-exports TypeDI decorators and Injectable alias for use throughout the codebase.
- */
-import { Service } from 'typedi';
-
-export const Injectable = Service;
-export * from 'typedi';
